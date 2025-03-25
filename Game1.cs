53c48064a634a63ee8f294b39f09b68705053781
Zoey Hamm <339622581@GoTVDSB.ca>
2025-03-25 14:43:54 -0400

    Finished tutorial... On to assignment!

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;  //Imports list class...
 
 namespace Monogame_1___Recap
 {
@@ -9,11 +11,14 @@ namespace Monogame_1___Recap
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Texture2D backgroundTexture, shipTexture, secondShipTexture, thirdShipTexture, fourthShipTexture, fifthShipTexture;
-        Rectangle window, shipRect, secondShipRect, thirdShipRect, fourthShipRect, fifthShipRect;
-        SpriteFont titleFont;
-        
+        Random generator;
 
+        Texture2D backgroundTexture, shipTexture, secondShipTexture, thirdShipTexture, fourthShipTexture, fifthShipTexture;
+        Rectangle window, shipRect, secondShipRect; /* thirdShipRect, fourthShipRect, fifthShipRect*/
+        SpriteFont titleFont;
+
+        List<Texture2D> shipTextures; 
+        
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -31,11 +36,15 @@ namespace Monogame_1___Recap
 
             this.Window.Title = "Content, Scaling, and Texts";
 
-            shipRect = new Rectangle(110, 300, 75, 100);
-            secondShipRect = new Rectangle(210, 300, 75, 100);
-            thirdShipRect = new Rectangle(310, 300, 75, 100);
-            fourthShipRect = new Rectangle(410, 300, 75, 100);
-            fifthShipRect = new Rectangle(510, 300, 75, 100);
+            generator = new Random();
+
+            shipRect = new Rectangle(generator.Next(0,window.Width - 75), generator.Next(0,window.Height-100), 75, 100);
+            secondShipRect = new Rectangle(generator.Next(0, window.Width - 75), generator.Next(0, window.Height - 100), 75, 100);
+
+            shipTextures = new List<Texture2D>();
+            //thirdShipRect = new Rectangle(310, 300, 75, 100);
+            //fourthShipRect = new Rectangle(410, 300, 75, 100);
+            //fifthShipRect = new Rectangle(510, 300, 75, 100);
 
 
             base.Initialize();
@@ -47,11 +56,18 @@ namespace Monogame_1___Recap
 
             // TODO: use this.Content to load your game content here
             backgroundTexture = Content.Load<Texture2D>("Images/space_background");
-            shipTexture = Content.Load<Texture2D>("Images/enterprise_1");
-            secondShipTexture = Content.Load<Texture2D>("Images/enterprise_2");
-            thirdShipTexture = Content.Load<Texture2D>("Images/enterprise_3");
-            fourthShipTexture = Content.Load<Texture2D>("Images/enterprise_4");
-            fifthShipTexture = Content.Load<Texture2D>("Images/enterprise_5");
+           
+
+            for (int i = 1; i <= 5; i++)
+            {
+                shipTextures.Add(Content.Load<Texture2D>("Images/enterprise_" + i));
+            }
+
+            shipTexture = shipTextures[generator.Next(shipTextures.Count)];
+            secondShipTexture = shipTextures[generator.Next(shipTextures.Count)];
+            //thirdShipTexture = Content.Load<Texture2D>("Images/enterprise_3");
+            //fourthShipTexture = Content.Load<Texture2D>("Images/enterprise_4");
+            //fifthShipTexture = Content.Load<Texture2D>("Images/enterprise_5");
 
             titleFont = Content.Load<SpriteFont>("Fonts/TitleFont");
         }
@@ -74,12 +90,14 @@ namespace Monogame_1___Recap
             _spriteBatch.Begin();
 
             _spriteBatch.Draw(backgroundTexture, window, Color.White);
-            _spriteBatch.DrawString(titleFont, "Space Exploration", new Vector2(150, 10), Color.YellowGreen);
+            _spriteBatch.DrawString(titleFont, "Space Exploration", new Vector2(110, 10), Color.YellowGreen);
             _spriteBatch.Draw(secondShipTexture, secondShipRect, Color.White);
-            _spriteBatch.Draw(shipTexture, shipRect, Color.White);
-            _spriteBatch.Draw(thirdShipTexture, thirdShipRect, Color.White);
-            _spriteBatch.Draw(fourthShipTexture, fourthShipRect, Color.White);
-            _spriteBatch.Draw(fifthShipTexture, fifthShipRect, Color.White);
+            _spriteBatch.Draw(shipTexture, shipRect, null, Color.White * 0.2f, 1f, Vector2.Zero, SpriteEffects.FlipVertically, 0f);
+
+
+            //_spriteBatch.Draw(thirdShipTexture, thirdShipRect, Color.White);
+            //_spriteBatch.Draw(fourthShipTexture, fourthShipRect, Color.White);
+            //_spriteBatch.Draw(fifthShipTexture, fifthShipRect, Color.White);
 
 
             _spriteBatch.End();
